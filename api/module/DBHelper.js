1af414ada1cad552a6e60a369b307967effabeb5
xzhzzz <793414851@qq.com>
2017-06-11 20:48:11 +0800

    goodsListcssmodify

--- api/module/DBHelper.js
+++ api/module/DBHelper.js
@@ -2,7 +2,7 @@ var ApiResult = require('./ApiResult')
 
 var MongoDB = require('mongodb');
 var MongoDBServer = new MongoDB.Server('localhost', 27017);
-var db = new MongoDB.Db('test', MongoDBServer);
+var db = new MongoDB.Db('babyApp', MongoDBServer);
 
 module.exports = {
     get: function(_collection, _condition, _callback){
--- web/src/modules/goodsList/GoodsListComponent.js
+++ web/src/modules/goodsList/GoodsListComponent.js
@@ -40,7 +40,68 @@ class GoodsListComponent extends React.Component {
                 </div>
                 <div className="goodsList-main">
                     <div className="main-section">
-                        123
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                    <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                     <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                     <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                     <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                     <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                     <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
+                    </div>
+                     <div className="main-section">
+                        <img src="../../../src/static/imgs/goods/p1.jpg"/>
+                        <div className="section-right">
+                            <p className="goodsName">美素佳儿金裝3段幼儿配方奶粉900g（美素冰点价（特供商品，不与其他活动同享））</p>
+                            <p className="goodsPrice">¥169.00</p>
+                            <p className="goodsSalenum">销量:2888</p>
+                        </div>
                     </div>
                 </div>
             </div>
